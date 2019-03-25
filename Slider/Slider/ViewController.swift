89344b59ac276df8cc7181fba99272b8b6f0ce51
abdullah1107 <abdullah.cse11@gmail.com>
2019-03-25 11:24:33 +0600

    adding slider

---
+++ Slider/Slider/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by Muhammad Abdullah Al Mamun on 24/3/19.
+//  Copyright © 2019 Muhammad Abdullah Al Mamun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func sliderAction(_ sender: UISlider) {
+      sliderLabel.text = String(Int(sender.value))
+    }
+    
+}
+
