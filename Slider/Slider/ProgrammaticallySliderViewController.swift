8cc18ae77e7ad09d16bc7a9ab36f418f9ff53e5f
abdullah1107 <abdullah.cse11@gmail.com>
2019-03-25 11:30:46 +0600

    change

---
+++ Slider/Slider/ProgrammaticallySliderViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProgrammaticallySliderViewController.swift
+//  Slider
+//
+//  Created by Muhammad Abdullah Al Mamun on 25/3/19.
+//  Copyright © 2019 Muhammad Abdullah Al Mamun. All rights reserved.
+//
+
+import UIKit
+
+class ProgrammaticallySliderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+ 
+
+}
--- Slider/Slider/StoryboardSliderViewController.swift
+++ Slider/Slider/StoryboardSliderViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StoryboardSliderViewController: UIViewController {
     
     
     @IBOutlet weak var sliderLabel: UILabel!
