fb539b7978abd632316c8e1df76eb04e15b768d5
abdullah1107 <abdullah.cse11@gmail.com>
2019-03-25 12:09:03 +0600

    new adding

--- Slider/Slider/ProgrammaticallySliderViewController.swift
+++ Slider/Slider/ProgrammaticallySliderViewController.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class ProgrammaticallySliderViewController: UIViewController {
 
+    let slider = UISlider()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        slider.frame = CGRect(x:0, y:0, width:250, height:35)
+        slider.center = self.view.center
+        
+        slider.minimumTrackTintColor = .green
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .black
+        
+        slider.maximumValue = 100
+        slider.minimumValue = 0
+        slider.setValue(50, animated: false)
+        
+        
+        slider.addTarget(self, action: #selector(ProgrammaticallySliderViewController.changeVlaue(_:)), for: .valueChanged)
+        
+        self.view.addSubview(slider)
+        
 
         
+    }
+    
+    @objc func changeVlaue(_ sender: UISlider) {
+        print("value is" , Int(sender.value));
     }
     
 
