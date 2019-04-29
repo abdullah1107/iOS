87ac0aaf051888312a69dad0694bd83ae93c5b14
abdullah1107 <abdullah.cse11@gmail.com>
2019-04-29 21:24:42 +0600

    adding custom Color

--- PinchGesture/PinchGesture/ViewController.swift
+++ PinchGesture/PinchGesture/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var viewPinch: UIView!
+    
+    
+    var pinchGesture  = UIPinchGestureRecognizer()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(self.pinchedView))
+        viewPinch.isUserInteractionEnabled = true
+        viewPinch.addGestureRecognizer(pinchGesture)
+    }
+    
+    @objc func pinchedView(sender:UIPinchGestureRecognizer){
+        self.view.bringSubviewToFront(viewPinch)
+        sender.view?.transform = (sender.view?.transform)!.scaledBy(x: sender.scale, y: sender.scale)
+        sender.scale = 3.0
     }
 
 
